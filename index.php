e3ae8c6ba266521b8bb4caa661c0a334cf144ed7
Don Myers <myersd@trueaction.com>
2013-02-07 13:02:04 -0500

    remove unused

--- index.php
+++ index.php
@@ -4,11 +4,9 @@
 		<meta charset="utf-8">
 		<title>HTML5 Basic Template</title>
 		<link href="/assets/bootstrap/css/bootstrap.css" type="text/css" rel="stylesheet">
-		<link href="/assets/css/site.css" type="text/css" rel="stylesheet">
 
-		<script type="text/javascript" src="/assets/js/jquery/jquery.1.8.2.js"></script>
+		<script type="text/javascript" src="/assets/jquery/jquery.1.8.2.js"></script>
 		<script type="text/javascript" src="/assets/bootstrap/js/bootstrap.js"></script>
-		<script type="text/javascript" src="/assets/js/site.js"></script>
 
     <script type="text/javascript" >
     $(function () {
@@ -50,7 +48,7 @@
 		  	
 		  	/* patch in a few overriding functions */
 		  	$(this).data('typeahead').highlighter = function (item) { return '<div>' + item + '</div>'; }
-		  	$(this).data('typeahead').matcher = function (item) { return true; }
+		  	$(this).data('typeahead').matcher = function(item) { if (this.query == '*') { return true; } else { return item.indexOf(this.query) >= 0; } }
 		  	
 	    	$(this).wrap('<div class="input-append">')
 	    					.after('<div class="btn-group"><a class="btn dropdown-toggle"><span class="caret"></span></a></div>')
@@ -58,10 +56,12 @@
 	    					.click(function(e) {
 	    						/* pause longer than bootstrap so the dropdown does cause the input field to lose focus 150 */
 	    						setTimeout(function () {
-										var items = $(that).data('typeahead').options.items;
-	    							$(that).data('typeahead').options.items = 96;
-										$(that).val('a').typeahead('lookup').val('');
-	    							$(that).data('typeahead').options.items = items;
+	    							var options = $(that).data('typeahead').options;
+										var items = options.items;
+										/* when just using the drop down select a maximum of 128 */
+	    							options.items = 128;
+										$(that).val('*').typeahead('lookup').val('');
+	    							options.items = items;
 									}, 160);
 								});
 		  };
