21d307b1c64fac7d2554fd598160f13c033ca450
Don Myers <myersd@trueaction.com>
2013-03-18 15:52:52 -0400

    updated to handle classes

--- index.php
+++ index.php
@@ -3,97 +3,156 @@
 	<head>
 		<meta charset="utf-8">
 		<title>HTML5 Basic Template</title>
-		<link href="/assets/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+		<link href="assets/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 
-		<script type="text/javascript" src="/assets/jquery/jquery.1.8.3.min.js"></script>
-		<script type="text/javascript" src="/assets/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/jquery/jquery.1.8.3.min.js"></script>
+		<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/combo.js"></script>
 
-    <script type="text/javascript" >
-    $(function () {
-
-        var jazz_musicians = [
-          "Scott Joplin",
-          "Charles Bolden",
-          "Duke Ellington",
-          "Louis Armstrong",
-          "Earl Hines",
-          "Fats Waller",
-          "Count Basie",
-          "Benny Goodman",
-          "Sun Ra",
-          "Thelonious Monk",
-          "Dizzy Gillespie",
-          "Charlie Parker",
-          "Dave Brubeck",
-          "Charles Mingus",
-          "Oscar Peterson",
-          "Miles Davis",
-          "John Coltrane",
-          "Chet Baker",
-          "Ornette Coleman",
-          "Wynton Marsalis",
-          "Billie Holiday",
-          "Ella Fitzgerald",
-          "Sarah Vaughan"
-        ];
-
-			$('#typeahead').typeahead({ source: jazz_musicians });
-			
-			$('#typeahead-combo').typeahead({ source: jazz_musicians }).combobox();
-			
-			$('#typeahead-combo-ajax').typeahead({ source: function(query, process) {
-					return $.get('typeahead.php', { query: query }, function (data) {
-						return process(data.options);
-					});
-				}
-			}).combobox();
-
-    });
-    
-    (function($){
-		  $.fn.combobox = function(options) {
-		  	var that = this;
-		  	
-		  	/*
-		  	If it's set for 8 then they prob left it at the default set it to 128
-		  	The dropdown is kind-a worthless if it's only showing 8
-		  	*/
-		  	var options = $(this).data('typeahead').options;
-		  	options.items = (options.items == 8 ) ? 128 : options.items;
-		  	
-		  	/* patch in a few overriding functions */
-		  	$(this).data('typeahead').highlighter = function (item) { return '<div>' + item + '</div>'; }
-		  	$(this).data('typeahead').matcher = function(item) { if (this.query == '*') { return true; } else { return item.indexOf(this.query) >= 0; } }
-		  	
-	    	$(this).wrap('<div class="input-append">')
-	    					.after('<div class="btn-group"><a class="btn dropdown-toggle"><span class="caret"></span></a></div>')
-	    					.next()
-	    					.click(function(e) {
-	    						/* pause longer than bootstrap so the dropdown does cause the input field to lose focus 150 */
-	    						setTimeout(function () {
-										$(that).val('*').typeahead('lookup').val('');
-									}, 160);
-								});
-		  };
-		})(jQuery);
-    
-    </script>
+    <style>
+    	html {
+    		overflow-y: scroll;
+    	}
+    </style>
 	</head>
 	<body>
 		<div class="container showgrid">
 			<p><br></p>
+
 			<p>
-				<input id="typeahead" class="span2" type="text">
+				Type Ahead<br>
+				<input type="text" name="foo" id="typeahead">
 			</p>
 
 			<p>
-				<input id="typeahead-combo" class="span2" type="text">
+				Type Ahead Callback<br>
+				<input type="text" name="bar" id="typeaheadcallback">
+			</p>
+			
+			<p>
+				Type Ahead Combo Class<br>
+				<input class="combobox" type="text" name="aaa">
 			</p>
 
 			<p>
-				<input id="typeahead-combo-ajax" class="span3" type="text">
+				Type Ahead Combo Class<br>
+				<input class="combobox" type="text" name="bbb">
 			</p>
 
+			
+			<p>
+				Type Ahead Select Class<br>			
+				<select name="stateb" class="selectcombo">
+					<option value="AL">Alabama</option>
+					<option value="AK">Alaska</option>
+					<option value="AZ">Arizona</option>
+					<option value="AR">Arkansas</option>
+					<option value="CA">California</option>
+					<option value="CO">Colorado</option>
+					<option value="CT">Connecticut</option>
+					<option value="DE">Delaware</option>
+					<option value="DC">District Of Columbia</option>
+					<option value="FL">Florida</option>
+					<option value="GA">Georgia</option>
+					<option value="HI" selected>Hawaii</option>
+					<option value="ID">Idaho</option>
+					<option value="IL">Illinois</option>
+					<option value="IN">Indiana</option>
+					<option value="IA">Iowa</option>
+					<option value="KS">Kansas</option>
+					<option value="KY">Kentucky</option>
+					<option value="LA">Louisiana</option>
+					<option value="ME">Maine</option>
+					<option value="MD">Maryland</option>
+					<option value="MA">Massachusetts</option>
+					<option value="MI">Michigan</option>
+					<option value="MN">Minnesota</option>
+					<option value="MS">Mississippi</option>
+					<option value="MO">Missouri</option>
+					<option value="MT">Montana</option>
+					<option value="NE">Nebraska</option>
+					<option value="NV">Nevada</option>
+					<option value="NH">New Hampshire</option>
+					<option value="NJ">New Jersey</option>
+					<option value="NM">New Mexico</option>
+					<option value="NY">New York</option>
+					<option value="NC">North Carolina</option>
+					<option value="ND">North Dakota</option>
+					<option value="OH">Ohio</option>
+					<option value="OK">Oklahoma</option>
+					<option value="OR">Oregon</option>
+					<option value="PA">Pennsylvania</option>
+					<option value="RI">Rhode Island</option>
+					<option value="SC">South Carolina</option>
+					<option value="SD">South Dakota</option>
+					<option value="TN">Tennessee</option>
+					<option value="TX">Texas</option>
+					<option value="UT">Utah</option>
+					<option value="VT">Vermont</option>
+					<option value="VA">Virginia</option>
+					<option value="WA">Washington</option>
+					<option value="WV">West Virginia</option>
+					<option value="WI">Wisconsin</option>
+					<option value="WY">Wyoming</option>
+				</select>
+			</p>
+
+			<p>
+				Type Ahead Select Class<br>			
+				<select name="statea" class="selectcombo">
+					<option value="AL">Alabama</option>
+					<option value="AK">Alaska</option>
+					<option value="AZ">Arizona</option>
+					<option value="AR">Arkansas</option>
+					<option value="CA">California</option>
+					<option value="CO">Colorado</option>
+					<option value="CT">Connecticut</option>
+					<option value="DE">Delaware</option>
+					<option value="DC">District Of Columbia</option>
+					<option value="FL">Florida</option>
+					<option value="GA">Georgia</option>
+					<option value="HI" selected>Hawaii</option>
+					<option value="ID">Idaho</option>
+					<option value="IL">Illinois</option>
+					<option value="IN">Indiana</option>
+					<option value="IA">Iowa</option>
+					<option value="KS">Kansas</option>
+					<option value="KY">Kentucky</option>
+					<option value="LA">Louisiana</option>
+					<option value="ME">Maine</option>
+					<option value="MD">Maryland</option>
+					<option value="MA">Massachusetts</option>
+					<option value="MI">Michigan</option>
+					<option value="MN">Minnesota</option>
+					<option value="MS">Mississippi</option>
+					<option value="MO">Missouri</option>
+					<option value="MT">Montana</option>
+					<option value="NE">Nebraska</option>
+					<option value="NV">Nevada</option>
+					<option value="NH">New Hampshire</option>
+					<option value="NJ">New Jersey</option>
+					<option value="NM">New Mexico</option>
+					<option value="NY">New York</option>
+					<option value="NC">North Carolina</option>
+					<option value="ND">North Dakota</option>
+					<option value="OH">Ohio</option>
+					<option value="OK">Oklahoma</option>
+					<option value="OR">Oregon</option>
+					<option value="PA">Pennsylvania</option>
+					<option value="RI">Rhode Island</option>
+					<option value="SC">South Carolina</option>
+					<option value="SD">South Dakota</option>
+					<option value="TN">Tennessee</option>
+					<option value="TX">Texas</option>
+					<option value="UT">Utah</option>
+					<option value="VT">Vermont</option>
+					<option value="VA">Virginia</option>
+					<option value="WA">Washington</option>
+					<option value="WV">West Virginia</option>
+					<option value="WI">Wisconsin</option>
+					<option value="WY">Wyoming</option>
+				</select>
+			</p>
 
 		</div>
 	</body>
